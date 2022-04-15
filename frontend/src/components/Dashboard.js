00d46d4e1057e5ff6985c5d1545c13c163f29138
chekjun <limchekjun@gmail.com>
2022-04-15 14:47:26 +0800

    Update Dashboard.js

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,99 +3,78 @@ import '../styles/Dashboard.scss';
 import MultiChart from './MultiChart.tsx';
 
 import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem'
 import Select from '@mui/material/Select';
 import Stack from '@mui/material/Stack';
-import CssBaseline from '@mui/material/CssBaseline';
+
 
 function Dashboard(props) {
   const [yAxis, setYAxis] = React.useState('');
   const [y1Axis, setY1Axis] = React.useState('');
-  const [xAxis, setXAxis] = React.useState('');
 
-  const selectGraphOptions = (
+  const graphSelection = (
     <Stack
       alignItems="center" 
       direction="row"
       spacing="100px"
     >
-      <Select
-          id="Y-Axis"
-          value={yAxis}
-          label="Y-Axis"
+      <FormControl fullWidth>
+        <InputLabel id="yAxisLabel">Select Y-Axis</InputLabel>
+        <Select
+            labelId="yAxisLabel"
+            id="Y-Axis"
+            value={yAxis}
+            label="Y-Axis"
+            onChange={(event) => {
+              setYAxis(event.target.value);
+            }}
+          >
+            <MenuItem value={"floor_price"}>Floor Price</MenuItem>
+            <MenuItem value={"follower_count"}>Follower Count</MenuItem>
+            <MenuItem value={"like_count"}>Like Count</MenuItem>
+            <MenuItem value={"market_value"}>Market Value</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl fullWidth>
+        <InputLabel id="y1AxisLabel">Select Y1-Axis</InputLabel>
+        <Select
+          labelId="y1AxisLabel"
+          id="Y1-Axis"
+          value={y1Axis}
+          label="Y1-Axis"
           onChange={(event) => {
-            setYAxis(event.target.value);
+            setY1Axis(event.target.value);
           }}
         >
-          <MenuItem value={"floor_price"}>Floor Price</MenuItem>
-          <MenuItem value={"follower_count"}>Follower Count</MenuItem>
-          <MenuItem value={"like_count"}>Like Count</MenuItem>
-          <MenuItem value={"market_value"}>Market Value</MenuItem>
-      </Select>
-      <Select
-        id="Y1-Axis"
-        value={y1Axis}
-        label="Y1-Axis"
-        onChange={(event) => {
-          setY1Axis(event.target.value);
-        }}
-      >
-          <MenuItem value={"floor_price"}>Floor Price</MenuItem>
-          <MenuItem value={"follower_count"}>Follower Count</MenuItem>
-          <MenuItem value={"like_count"}>Like Count</MenuItem>
-          <MenuItem value={"market_value"}>Market Value</MenuItem>
-      </Select>
+            <MenuItem value={"floor_price"}>Floor Price</MenuItem>
+            <MenuItem value={"follower_count"}>Follower Count</MenuItem>
+            <MenuItem value={"like_count"}>Like Count</MenuItem>
+            <MenuItem value={"market_value"}>Market Value</MenuItem>
+        </Select>
+      </FormControl>
     </Stack>
-  )
+  );
+
 
   function Display() {
     if (props.collection.length === 0) {
       return (
-        <h1>
-          Please select at least one NFT.
-        </h1>
+        <h1> Please select at least one NFT. </h1>
       );
     } else if (props.collection.length === 1) {
-
-      const mockFollowerCount = [5364,3456,3443,1234,1234,3214,4000,5322,2313,7654]
-      const mockFloorPrice = [1.99, 2.00, 2.15, 1.01, 3.5, 4.8, 1.91, 5.53, 0.88, 1.02]
-      const mockNFTNames = ["Ayy", "Bee", "Cee", "Dee", "Ee", "Eff", "Gee", "Aitch", "Aye", "Jay"]
       return (
         <>
-          <h1>
-            ONE NFT SELECTED
-          </h1>
-          { selectGraphOptions }
-          <MultiChart
-            name={"MULTICHART"}
-            yData={mockFollowerCount}
-            yLabel={"Likes"}
-            y1Data={mockFloorPrice}
-            y1Label={"Floor Price"}
-            labels={mockNFTNames}
-          />
+          { graphSelection }
         </>
       );
     } else {
-      const mockFollowerCount = [5364,3456,3443,1234,1234,3214,4000,5322,2313,7654]
-      const mockFloorPrice = [1.99, 2.00, 2.15, 1.01, 3.5, 4.8, 1.91, 5.53, 0.88, 1.02]
-      const mockNFTNames = ["Ayy", "Bee", "Cee", "Dee", "Ee", "Eff", "Gee", "Aitch", "Aye", "Jay"]
       return (
         <>
-          <h1>
-            TWO NFTs SELECTED
-          </h1>
-          { selectGraphOptions }
-          <MultiChart
-            name={"MULTICHART"}
-            yData={mockFollowerCount}
-            yLabel={"Likes"}
-            y1Data={mockFloorPrice}
-            y1Label={"Floor Price"}
-            labels={mockNFTNames}
-          />
+          { graphSelection }
         </>
-
       );
     }
   }
@@ -130,3 +109,27 @@ const charts = () => {
 */
 // insert this for the timmy chart
 // { charts() }
+
+/*
+      const mockFollowerCount = [5364,3456,3443,1234,1234,3214,4000,5322,2313,7654]
+      const mockFloorPrice = [1.99, 2.00, 2.15, 1.01, 3.5, 4.8, 1.91, 5.53, 0.88, 1.02]
+      const mockNFTNames = ["Ayy", "Bee", "Cee", "Dee", "Ee", "Eff", "Gee", "Aitch", "Aye", "Jay"]
+
+          <MultiChart
+            name={"MULTICHART"}
+            yData={mockFollowerCount}
+            yLabel={"Likes"}
+            y1Data={mockFloorPrice}
+            y1Label={"Floor Price"}
+            labels={mockNFTNames}
+          />
+
+                    <MultiChart
+            name={"MULTICHART"}
+            yData={mockFollowerCount}
+            yLabel={"Likes"}
+            y1Data={mockFloorPrice}
+            y1Label={"Floor Price"}
+            labels={mockNFTNames}
+          />
+          */
